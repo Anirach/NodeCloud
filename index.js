790f33aa14a4e52399a0520177b0c52beb5cf18f
Anirach Mingkhwan <Anirach.m@fitm.kmutnb.ac.th>
2020-12-25 09:02:30 +0700

    check code

--- index.js
+++ index.js
@@ -5,11 +5,7 @@ const app = express();
 const port = process.env.PORT;
 
 app.get("/", (req, res) => {
-<<<<<<< HEAD
   res.send("Hello Node.js World! it is done");
-=======
-  res.send("Hello Node.js Changing..World!");
->>>>>>> b4574a22d0d811d85e8b23e1ed535d2cafc4f8c9
 });
 
 app.listen(port, () => {
